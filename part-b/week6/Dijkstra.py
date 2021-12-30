5a8027f85e357ec4dd533c26353d10c3635cea54
Luis Antonio Momm Duarte <luismomm@gmail.com>
2021-12-29 22:34:49 -0300

    find min

--- part-b/week6/Dijkstra.py
+++ part-b/week6/Dijkstra.py
@@ -1,19 +1,42 @@
-from WeightedGraph import WeightedGraph
+from os import minor
+
 
 def fill_graph(textFile, graph):
-	f = open(textFile, "r")
-	lines = f.readlines()
+    f = open(textFile, "r")
+    lines = f.readlines()
 
-	for line in lines:
-		line = line.split("\t")
-		for edge in line[-1]:
-			print(edge.split(","))
+    for line in lines:
+        line = line.split("\t")
+        vertex = line[0]
+        graph[vertex] = []
+
+        for edge in line[1:-1]:
+            graph[vertex].append(list(map(lambda x: int(x), edge.split(","))))
+
+    return graph
+
+
+def dij_loop(g, vertices_processed_so_far, computed_distances):
+    for vertex in g.keys():
+        if vertex not in vertices_processed_so_far:
+            vertices_processed_so_far.append(vertex)
+            for edge in g[vertex]:
+                computed_distances[edge[0]-1] = edge[1]
 
 
 def main():
-	g = WeightedGraph()
+    g = dict()
 
-	fill_graph("graph.txt", g)
+    g = fill_graph("graph.txt", g)
+    v = 1
 
-if __name__ == "__main__": 
-	main()
+    vertices_processed_so_far = []
+    computed_distances = [10000]*len(g.keys())
+    computed_distances[v-1] = 0
+
+    dij_loop(g, vertices_processed_so_far, computed_distances)
+    print(computed_distances[80])
+
+
+if __name__ == "__main__":
+    main()
--- part-b/week6/WeightedGraph.py
+++
@@ -1,7 +0,0 @@
-class WeightedGraph:
-    def __init__(self):
-        self.graph = []
-
-
-    def addEdge(self, u, weightedEdge):
-        self.graph[u] = weightedEdge
