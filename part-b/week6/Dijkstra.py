577406b8e2ae81f6941abd12ff3603d50baac2ee
luismomm2110 <luismomm@gmail.com>
2021-12-30 13:52:01 -0300

    finish lab5

--- part-b/week6/Dijkstra.py
+++ part-b/week6/Dijkstra.py
@@ -1,13 +1,10 @@
-from os import minor
-
-
 def fill_graph(textFile, graph):
     f = open(textFile, "r")
     lines = f.readlines()
 
     for line in lines:
         line = line.split("\t")
-        vertex = line[0]
+        vertex = int(line[0])
         graph[vertex] = []
 
         for edge in line[1:-1]:
@@ -16,12 +13,43 @@ def fill_graph(textFile, graph):
     return graph
 
 
+def find_min_edge(g, computed_distances, vertices_processed_so_far):
+    current_index = 1
+    min_distance = 100000
+
+    for i in range(len(computed_distances)):
+
+        if not vertices_processed_so_far[i] and computed_distances[i] <= min_distance:
+            min_distance = computed_distances[i]
+            current_index = i + 1
+
+    return current_index, min_distance
+
+
+def update_distances(g, computed_distances, processed_vertex, vertices_processed_so_far):
+    for edge in g[processed_vertex]:
+        vertex = edge[0]
+        weight = edge[1] + computed_distances[processed_vertex-1]
+
+        if not vertices_processed_so_far[vertex-1] and weight < computed_distances[vertex-1]:
+            computed_distances[vertex -
+                               1] = weight
+
+    return computed_distances
+
+
 def dij_loop(g, vertices_processed_so_far, computed_distances):
-    for vertex in g.keys():
-        if vertex not in vertices_processed_so_far:
-            vertices_processed_so_far.append(vertex)
-            for edge in g[vertex]:
-                computed_distances[edge[0]-1] = edge[1]
+
+    while not all(vertices_processed_so_far):
+        processed_vertex, min_distance = find_min_edge(
+            g, computed_distances, vertices_processed_so_far)
+
+        vertices_processed_so_far[processed_vertex-1] = True
+
+        computed_distances = update_distances(
+            g, computed_distances, processed_vertex, vertices_processed_so_far)
+
+    return computed_distances
 
 
 def main():
@@ -30,12 +58,16 @@ def main():
     g = fill_graph("graph.txt", g)
     v = 1
 
-    vertices_processed_so_far = []
-    computed_distances = [10000]*len(g.keys())
+    vertices_processed_so_far = [False]*len(g.keys())
+    computed_distances = [100000]*len(g.keys())
+
     computed_distances[v-1] = 0
 
-    dij_loop(g, vertices_processed_so_far, computed_distances)
-    print(computed_distances[80])
+    computed_distances = dij_loop(
+        g, vertices_processed_so_far, computed_distances)
+
+    for index in [7, 37, 59, 82, 99, 115, 144, 165, 188, 197]:
+        print(computed_distances[index-1], end=",")
 
 
 if __name__ == "__main__":
