350e65d9104eee444ee4fa759ffb04be2d94aece
luismomm2110 <luismomm@gmail.com>
2022-01-06 08:06:10 -0300

    creating extract min

--- part-b/week7/Heap.py
+++ part-b/week7/Heap.py
@@ -18,6 +18,24 @@ class Heap:
             parent_index = self.findParentIndex(inserted_index)
             parent_value = self.heap[parent_index]
 
+    def extract_min(self):
+        min = self.heap[0]
+
+        self.heap[0] = self.heap[-1]
+
+        index = 0
+
+        left_child = self.heap[1]
+        right_child = self.heap[2]
+
+        children = {1: left_child, 2: right_child}
+
+        while (self.heap[index] > sorted(children.values()[0])):
+            temp = self.heap[index]
+            new_key = children.keys().index(sorted(children.values()[0]))
+
+        return min
+
     def findParentIndex(self, i):
         if (i-1) % 2 == 0:
             return int(i/2)
